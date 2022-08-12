6cbb76c5812cf1153a1c72feb52982ef50bfbc0f
Maaz <syedmaaz508@gmail.com>
2022-08-13 02:32:44 +0500

    first commit

---
+++ intro.js
@@ -0,0 +1,10 @@
+const IntroContainer=document.querySelector(".intro")
+
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+    setTimeout(() => {
+        IntroContainer.classList.add("fading")
+    }, 3000);
+})
+
+
